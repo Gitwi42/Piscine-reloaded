8a91f3eb2ecb6ce81dc8c4c4e1b8a1fa0cc69918
root <root@DESKTOP-3HCAAK3.localdomain>
2026-08-28 14:29:28 +0200

    ex14 optimisation

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -6,22 +6,20 @@
 /*   By: root <mhinojos@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/28 14:01:35 by root              #+#    #+#             */
-/*   Updated: 2026/08/28 14:17:35 by root             ###   ########.fr       */
+/*   Updated: 2026/08/28 14:29:04 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 
 int	ft_sqrt(int nb)
 {
 	int	n;
 
-	if (nb == 1)
-		return (1);
 	if (nb < 1)
 		return (0);
 	n = 1;
-	while ((nb / 2) >= n)
+	while (n * n <= nb)
 	{
 		if (n * n == nb)
 			return (n);
@@ -30,11 +28,11 @@ int	ft_sqrt(int nb)
 	return (0);
 }
 
-/*int	main(void)
+int	main(void)
 {
-	printf("%d\n", ft_sqrt(1));
-	printf("%d\n", ft_sqrt(-36));
-	printf("%d\n", ft_sqrt(3));
-	printf("%d\n", ft_sqrt(9));
-	printf("%d\n", ft_sqrt(36));
-}*/
+	printf("%d\n", ft_sqrt(16));
+	printf("%d\n", ft_sqrt(25));
+	printf("%d\n", ft_sqrt(49));
+	printf("%d\n", ft_sqrt(50));
+	printf("%d\n", ft_sqrt(100));
+}

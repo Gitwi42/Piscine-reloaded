a34cf922082ccdfa8c9e1d3892d9ff674d840917
root <root@DESKTOP-3HCAAK3.localdomain>
2026-08-29 13:37:41 +0200

    adding ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <mhinojos@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/29 13:23:45 by root              #+#    #+#             */
+/*   Updated: 2026/08/29 13:34:38 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int	i;
+	char	*dest;
+
+	i = ft_strlen(src) + 1;
+	dest = malloc(sizeof(char) * i);
+	if (!dest)
+		return (0);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

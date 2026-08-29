37ca7b6646bf2450435a94351af27bf993aa32b5
root <root@DESKTOP-3HCAAK3.localdomain>
2026-08-29 14:04:02 +0200

    adding ex21

--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -1,42 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <mhinojos@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/29 13:23:45 by root              #+#    #+#             */
-/*   Updated: 2026/08/29 14:01:44 by root             ###   ########.fr       */
+/*   Created: 2026/08/29 13:38:13 by root              #+#    #+#             */
+/*   Updated: 2026/08/29 14:03:33 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+int	*ft_range(int min, int max)
 {
+	int	*total;
+	int	count;
 	int	i;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(char *src)
-{
-	int		i;
-	char	*dest;
-
-	i = ft_strlen(src) + 1;
-	dest = malloc(sizeof(char) * i);
-	if (!dest)
+	if (min >= max)
+	{
+		total = NULL;
+		return (total);
+	}
+	count = max - min;
+	total = malloc (sizeof (int) * count);
+	if (!total)
 		return (0);
 	i = 0;
-	while (src[i])
+	while (i != count)
 	{
-		dest[i] = src[i];
+		total[i] = min;
+		min++;
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	return (total);
 }

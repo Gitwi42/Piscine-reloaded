993f30f12455a081c0c731e3e17c446148901499
root <root@DESKTOP-3HCAAK3.localdomain>
2026-08-30 17:00:34 +0200

    last patch

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: root <mhinojos@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/30 15:35:21 by root              #+#    #+#             */
-/*   Updated: 2026/08/30 16:26:07 by root             ###   ########.fr       */
+/*   Updated: 2026/08/30 17:00:09 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ void	ft_puterr(char *str)
 
 int	main(int argc, char **argv)
 {
-	int	fd;
-	char	buffer[256];
-	ssize_t	byte_read;
+	int			fd;
+	char		buffer[256];
+	ssize_t		byte_read;
 
 	if (argc == 1)
 		ft_puterr("File name missing.\n");
@@ -38,8 +38,12 @@ int	main(int argc, char **argv)
 			ft_puterr("Cannot read file.\n");
 			return (0);
 		}
-		while ((byte_read = read(fd, buffer, 256)) > 0)
+		byte_read = read(fd, buffer, 256);
+		while (byte_read > 0)
+		{
 			write(1, buffer, byte_read);
+			byte_read = read(fd, buffer, 256);
+		}
 		close(fd);
 	}
 	else
